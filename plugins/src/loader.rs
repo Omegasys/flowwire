c51fab26a55e9dbfec8e549a086fd9b81a219f38
Omega <emomega@proton.me>
2026-06-06 23:40:52 -0400

    Create loader.rs

---
+++ plugins/src/loader.rs
@@ -0,0 +1,25 @@
+use libloading::{Library, Symbol};
+use anyhow::Result;
+use crate::api::FlowWirePlugin;
+use std::path::PathBuf;
+
+/// Represents a loaded plugin
+pub struct Plugin {
+    pub lib: Library,
+    pub instance: Box<dyn FlowWirePlugin + Send + Sync>,
+}
+
+pub struct Loader;
+
+impl Loader {
+    pub fn load(path: PathBuf) -> Result<Plugin> {
+        unsafe {
+            let lib = Library::new(&path)?;
+            let constructor: Symbol<unsafe fn() -> *mut dyn FlowWirePlugin> =
+                lib.get(b"_create_plugin")?;
+            let boxed_raw = constructor();
+            let instance = Box::from_raw(boxed_raw);
+            Ok(Plugin { lib, instance })
+        }
+    }
+}
